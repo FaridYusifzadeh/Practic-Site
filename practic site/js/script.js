e632c212d079e373090b96a0b316e494538a2150
FaridYusifzadeh <fariddsx2015@gmail.com>
2019-05-16 16:35:49 +0400

    Practics Site

---
+++ practic site/js/script.js
@@ -0,0 +1,12 @@
+document.getElementById("popup").style.width = "0";
+
+document.getElementById("closeid").onclick= function() {
+   document.getElementById("popup").style.width = "0";
+}
+
+
+   
+document.getElementById("tt").onclick=function(){
+   document.getElementById("popup").style.width = "250px";
+   
+}
